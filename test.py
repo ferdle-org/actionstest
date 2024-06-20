47f25bf0860d0b0787a59e3d1e55d509fa9f191a
Fernando Ruiz Diaz <88462636+ferdlestier@users.noreply.github.com>
2024-06-20 09:27:37 -0400

    Create test.py
    Signed-off-by: Fernando Ruiz Diaz <88462636+ferdlestier@users.noreply.github.com>

---
+++ test.py
@@ -0,0 +1,3 @@
+#completely revoked secret key used for testing purposes on pi repo
+AWS_ACCESS_KEY_ID_TEMP = "AKIA4UYYVYAARXI2EAO4"
+AWS_SECRET_ACCESS_KEY_TEMP = "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"
